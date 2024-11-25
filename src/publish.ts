3a4f10155ebe5c3b246a74b0bcbd7cb5bf0f9115
Kevin Dean <Kevin.Dean@datadevelopment.com>
2024-11-25 16:06:18 -0500

    Whatever.

--- src/publish.ts
+++ src/publish.ts
@@ -1,5 +1,6 @@
 import { spawnSync } from 'child_process';
 import * as fs from "fs";
+import * as path from "node:path";
 import config from "../config/publish.json" assert { type: "json" };
 
 type JSONAsRecord = Record<string, unknown>;
@@ -23,9 +24,25 @@ function run(captureOutput: boolean, command: string, ...args: string[]): string
     return captureOutput ? spawnResult.stdout.toString().split("\n").slice(0, -1) : [];
 }
 
-async function publish(): Promise<void> {
+try {
+    const publisherPath = path.resolve(".");
+
+    const stateFilePath = path.resolve(publisherPath, "config/publish.state.json");
+
+    let repositoryStates: Record<string, string | undefined> = {};
+
+    if (fs.existsSync(stateFilePath)) {
+        fs.readFile(stateFilePath, null, (err, data) => {
+            if (err !== null) {
+                throw err;
+            }
+
+            repositoryStates = JSON.parse(data.toString());
+        });
+    }
+
     for (const repository of config.repositories) {
-        console.log(`Repository ${repository}`);
+        console.log(`Repository ${repository}...`);
 
         process.chdir(`../${repository}`);
 
@@ -33,47 +50,84 @@ async function publish(): Promise<void> {
             throw new Error("Repository is not on main branch");
         }
 
-        if (run(true, "git", "status", "--short").length !== 0) {
-            throw new Error("Repository has uncommitted changes");
+        if (repositoryStates[repository] === undefined) {
+            if (run(true, "git", "status", "--short").length !== 0) {
+                throw new Error("Repository has uncommitted changes");
+            }
         }
 
-        await fs.promises.readFile("package.json").then((buffer) => {
-            return JSON.parse(buffer.toString()) as Record<string, unknown>;
-        }).then((packageConfig) => {
-            packageConfig["version"] = config.version;
+        function step(state: string, callback: () => void) {
+            let repositoryState = repositoryStates[repository];
 
-            const organizationPrefix = `@${config.organization}/`;
-            const dependencyVersion = `^${config.version}`;
+            if (repositoryState === undefined || repositoryState === state) {
+                repositoryStates[repository] = state;
 
-            function updateDependencies(dependencies: UndefinableJSONAsRecord) {
-                if (dependencies !== undefined) {
-                    for (const key in dependencies) {
-                        if (key.startsWith(organizationPrefix)) {
-                            dependencies[key] = dependencyVersion;
+                try {
+                    callback();
+
+                    delete repositoryStates[repository];
+                } finally {
+                    fs.writeFile(stateFilePath, `${JSON.stringify(repositoryStates, null, 2)}\n`, (err) => {
+                        if (err !== null) {
+                            throw err;
+                        }
+                    });
+                }
+            }
+        }
+
+        step("package", () => {
+            const packageConfigPath = path.resolve("package.json");
+
+            fs.readFile(packageConfigPath, null, (err, data) => {
+                if (err !== null) {
+                    throw err;
+                }
+
+                const packageConfig: JSONAsRecord = JSON.parse(data.toString());
+
+                packageConfig["version"] = config.version;
+
+                const organizationPrefix = `@${config.organization}/`;
+                const dependencyVersion = `^${config.version}`;
+
+                function updateDependencies(dependencies: UndefinableJSONAsRecord) {
+                    if (dependencies !== undefined) {
+                        for (const key in dependencies) {
+                            if (key.startsWith(organizationPrefix)) {
+                                dependencies[key] = dependencyVersion;
+                            }
                         }
                     }
                 }
-            }
 
-            updateDependencies(packageConfig["devDependencies"] as UndefinableJSONAsRecord);
-            updateDependencies(packageConfig["dependencies"] as UndefinableJSONAsRecord);
+                updateDependencies(packageConfig["devDependencies"] as UndefinableJSONAsRecord);
+                updateDependencies(packageConfig["dependencies"] as UndefinableJSONAsRecord);
 
-            return fs.promises.writeFile("package.json", `${JSON.stringify(packageConfig, null, 2)}\n`);
-        }).then(() => {
+                fs.writeFile(packageConfigPath, `${JSON.stringify(packageConfig, null, 2)}\n`, (err) => {
+                    if (err !== null) {
+                        throw err;
+                    }
+                });
+            });
+        });
+
+        step("npm install", () => {
             run(false, "npm", "install");
+        });
 
+        step("git commit", () => {
             run(false, "git", "commit", "--all", `--message=\"Updated to version ${config.version}\"`);
+        });
+
+        step("git tag", () => {
             run(false, "git", "tag", `v${config.version}`);
+        });
+
+        step("git push", () => {
             run(false, "git", "push", "--tags");
-
-            // Wait for tag workflow to run.
-            // Wait for release workflow to run, if it exists.
-
-            console.log("Done.");
         });
     }
+} catch (e) {
+    console.error(e);
 }
-
-await publish().catch(reason => {
-    console.log(reason);
-});
